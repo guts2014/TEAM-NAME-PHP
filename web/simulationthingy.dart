f9648ead818ae49aea753a922b99976681f41f2b
Ross Allan <rallanpcl@gmail.com>
2014-10-11 00:33:08 +0100

    Revert "classy shit"
    This reverts commit 6c04eb5f614131b08662128056fe8e57bf6aa32b.

--- web/simulationthingy.dart
+++ web/simulationthingy.dart
@@ -1,33 +1,20 @@
 import 'dart:html';
 
 class Game {
-  GameRenderer renderer;
-  
-  Game(this.renderer);
   
   
-  void run() {
-    renderer.getCell(1, 3).style.background = "green";
-  }
 }
 
 
-
-class GameRenderer {
-  Element getGameTable() {
-    return querySelector('#game');
-  }
-  Element getCell(num x, num y) {
-    var table = getGameTable();
-    return table.querySelectorAll('tr')[y].querySelectorAll('td')[x];
-  }
+Element getGameTable() {
+  return querySelector('#game');
 }
-
+Element getCell(x, y) {
+  var table = getGameTable();
+  return table.querySelectorAll('tr')[y].querySelectorAll('td')[x];
+}
 
 
 void main() {
-  GameRenderer renderer = new GameRenderer();
-  Game game = new Game(renderer);
-  
-  game.run();
+  getCell(2,3).style.background = "green";
 }
