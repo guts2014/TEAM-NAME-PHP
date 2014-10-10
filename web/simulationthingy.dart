6c04eb5f614131b08662128056fe8e57bf6aa32b
David Robertson <david@davidr.me>
2014-10-10 23:47:00 +0100

    classy shit

--- web/simulationthingy.dart
+++ web/simulationthingy.dart
@@ -1,20 +1,33 @@
 import 'dart:html';
 
 class Game {
+  GameRenderer renderer;
+  
+  Game(this.renderer);
   
   
+  void run() {
+    renderer.getCell(1, 3).style.background = "green";
+  }
 }
 
 
-Element getGameTable() {
-  return querySelector('#game');
+
+class GameRenderer {
+  Element getGameTable() {
+    return querySelector('#game');
+  }
+  Element getCell(num x, num y) {
+    var table = getGameTable();
+    return table.querySelectorAll('tr')[y].querySelectorAll('td')[x];
+  }
 }
-Element getCell(x, y) {
-  var table = getGameTable();
-  return table.querySelectorAll('tr')[y].querySelectorAll('td')[x];
-}
+
 
 
 void main() {
-  getCell(2,3).style.background = "green";
+  GameRenderer renderer = new GameRenderer();
+  Game game = new Game(renderer);
+  
+  game.run();
 }
